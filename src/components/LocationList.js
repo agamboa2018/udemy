9504238fd14c61f105f08ff601881efaf900c273
root <alvaro.gamboa@axont.com>
2019-05-20 17:59:14 -0300

    Seguimos

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import WeatherLocation from "./WeatherLocation/WeatherLocation";
+import "./styles.css";
 
 const LocationList = ({cities, onSelectedLocation}) => {
     const handleWeatherLocationClick = city => {
@@ -18,7 +19,7 @@ const LocationList = ({cities, onSelectedLocation}) => {
         )
     );
     return(
-        <div>
+        <div className="locationList">
             {strToComponent(cities)}
         </div>
     )
