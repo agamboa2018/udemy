ff374476d0b24e4591333d46f64f93d1b9191d83
root <alvaro.gamboa@axont.com>
2019-05-27 17:49:37 -0300

    Seguimos aprendiendo redux

--- src/container/ForecastExtendContainer.js
+++ src/container/ForecastExtendContainer.js
@@ -2,20 +2,21 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import ForecastExtended from "./../components/ForecastExtended";
 import { connect } from "react-redux";
+import { getForecastDataFromCities, getCity } from "./../reducers";
 
 class ForecastExtendContainer extends Component{
     render(){
+        const { city, forecastData } = this.props;
         return(
-            this.props.city && 
-            <ForecastExtended city={this.props.city}></ForecastExtended>
+            city && 
+            <ForecastExtended city={city} forecastData={forecastData}></ForecastExtended>
         )
     }
 }
 
 ForecastExtendContainer.protoTypes = {
-    city: PropTypes.string.isRequired
+    city: PropTypes.string.isRequired,
+    forecastData: PropTypes.array
 }
-const mapStateToProps = state =>({
-    city: state.city
-})
+const mapStateToProps = state => ({city: getCity(state), forecastData: getForecastDataFromCities(state) })
 export default connect(mapStateToProps, null)(ForecastExtendContainer);
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import reducer from "./../reducers/city";
+import reducers from "./../reducers/";
 
 const initialState = {
     city: "Buenos Aires, ar"
 };
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(reducer, initialState, composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(thunk)));
